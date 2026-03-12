13b3b681cfb15688f4f7cee756ad4b37d0904d32
Soujith <24eg107c52@anurag.edu.in>
2026-03-12 16:57:49 +0530

    Remove all files from git

--- Week-2/exercise1.js
+++
@@ -1,7 +0,0 @@
-let fruits = ["apple", "banana"];
-//copy of fruits
-let moreFruits=[...fruits,"orange"]//adding "orange" to copy of object
-
-//checking both arrays
-console.log(fruits)
-console.log(moreFruits)
