b6e204a13785c926625eaaeef385ec64830a74d1
Soujith <24eg107c52@anurag.edu.in>
2026-05-14 15:32:38 +0530

    latest commit

--- Week-1/Day-1/function1.js
+++ Week-1/Day-1/function1.js
@@ -1,10 +1,13 @@
 function findBig(a,b,c){
+    //check if a is big
     if(a>b && b>c){
         return "a is big"
     }
+    //check if b is big
     else if(b>c){
         return "b is big"
     }
+    //c is big
     else{
         return "c is big"
     }
